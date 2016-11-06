bac28125d5a36f8bb98092707cfbe5441080e1ec
ScoutOK <ohkeefe@gmail.com>
2016-11-06 13:11:34 -0500

    changed video element to canvas element

--- index.js
+++ index.js
@@ -8,6 +8,9 @@ let server = http.createServer();
 
 const app = express();
 
+var morgan = require('morgan');
+app.use(morgan('dev'));
+
 //requiring socket.io
 var socketio = require('socket.io');
 
@@ -19,7 +22,25 @@ server.listen(2222, function( server ) {
 
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', (req, res) => res.sendFile(__dirname + '/index.html'));
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
+
+//implement socket.io into the server
+var io = socketio(server);
+
+io.on('connection', (socket) => {
+    /* This function receives the newly connected socket.
+       This function will be called for EACH browser that connects to our server. */
+    socket.emit('previous', lines);
+    console.log('A new client has connected!');
+    socket.on('disconnect', () => console.log(':('));
+    socket.on('draw', (...data) => {
+      let [start, stop, strokeColor] = [...data];
+
+      lines.push({start: start, stop: stop, strokeColor: strokeColor});
+
+      socket.broadcast.emit('others', ...data);
+    })
+});
 
 
 
