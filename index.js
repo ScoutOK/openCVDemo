033d23c07595a8077f9496f3619cb89a2dc523f6
ScoutOK <ohkeefe@gmail.com>
2016-11-06 14:15:49 -0500

    face detection functional

--- index.js
+++ index.js
@@ -2,45 +2,36 @@
 
 const express = require('express');
 const router = require('./router');
-
+const faceDetect = require('./server/face-detect')
 const http = require('http');
-let server = http.createServer();
+const morgan = require('morgan');
+const socketio = require('socket.io');
+
+
+
 
 const app = express();
-
-var morgan = require('morgan');
+app.set('port', 2222);
+app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
 
 //requiring socket.io
-var socketio = require('socket.io');
-
+let server = http.createServer();
 server.on('request', app);
 
-server.listen(2222, function( server ) {
+server.listen(app.get('port'), function( server ) {
   console.log( 'Listening on port 2222' );
 });
 
-app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => res.sendFile(__dirname + '/public/index.html'));
 
 //implement socket.io into the server
 var io = socketio(server);
 
-io.on('connection', (socket) => {
-    /* This function receives the newly connected socket.
-       This function will be called for EACH browser that connects to our server. */
-    socket.emit('previous', lines);
-    console.log('A new client has connected!');
-    socket.on('disconnect', () => console.log(':('));
-    socket.on('draw', (...data) => {
-      let [start, stop, strokeColor] = [...data];
+io.on('connection', faceDetect);
 
-      lines.push({start: start, stop: stop, strokeColor: strokeColor});
-
-      socket.broadcast.emit('others', ...data);
-    })
-});
+module.exports.app = app;
 
 
 
